0c26f80d9b0f7fde3f7de949d515d70ea4e0d5b1
Clément Voisin <clembs@clembs.com>
2024-11-11 20:12:53 +0100

    feat: delete notification endpoint

---
+++ src/routes/api/notifications/+page.server.ts
@@ -0,0 +1,6 @@
+import type { Actions } from './$types';
+import { deleteNotification } from './delete-notification';
+
+export const actions: Actions = {
+	deleteNotification
+};
---
+++ src/routes/api/notifications/delete-notification.ts
@@ -0,0 +1,49 @@
+import { fail, redirect } from '@sveltejs/kit';
+import type { RequestEvent } from './$types';
+import { db } from '$lib/db';
+import {
+	notifications,
+	notificationsMentionedUsers,
+	NotificationTypes
+} from '$lib/db/schema/notifications';
+import { and, eq } from 'drizzle-orm';
+import { relationships, RelationshipTypes } from '$lib/db/schema/users';
+
+export async function deleteNotification({ url, locals: { getCurrentUser } }: RequestEvent) {
+	console.log('h');
+	const user = await getCurrentUser();
+
+	if (!user) redirect(302, '/login');
+
+	const notificationId = url.searchParams.get('id');
+	const notification = user.notifications.find((n) => n.id === notificationId);
+
+	if (!notificationId || !notification) {
+		return fail(400, {
+			message: 'Notification not found'
+		});
+	}
+
+	await db
+		.delete(notificationsMentionedUsers)
+		.where(eq(notificationsMentionedUsers.notificationId, notificationId));
+
+	await db.delete(notifications).where(eq(notifications.id, notificationId));
+
+	// if the notification was a friend request, delete the friend request relationship
+	if (notification.type === NotificationTypes.FriendRequest) {
+		const sourceUser = notification.mentionedUsers[0].user!;
+
+		await db
+			.delete(relationships)
+			.where(
+				and(
+					eq(relationships.recipientId, user.id),
+					eq(relationships.userId, sourceUser.id),
+					eq(relationships.status, RelationshipTypes.FriendPending)
+				)
+			);
+	}
+
+	return {};
+}
