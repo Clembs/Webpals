ae4696636533e8208bc5ae3630bb84818eba99bf
Clément Voisin <clembs@clembs.com>
2024-11-12 17:33:30 +0100

    feat: add all vars to theme provider

--- src/lib/themes/ThemeProvider.svelte
+++ src/lib/themes/ThemeProvider.svelte
@@ -33,27 +33,52 @@
 <theme-provider
 	style="
 	display: contents;
-  --avatar-size: {theme.avatar.size}px;
+	--background: {resolveBackground(theme)};
+  
+	--avatar-size: {theme.avatar.size}px;
   --avatar-border-radius: {typeof theme.avatar.border?.radius === 'string'
 		? theme.avatar.border?.radius
 		: `${theme.avatar.border?.radius}rem`};
-  --color-paragraph: {theme.font.color_paragraph};
+  
+	--color-paragraph: {theme.font.color_paragraph};
   --color-heading: {theme.font.color_heading};
   color: {theme.font.color_paragraph};
   font-family: '{theme.font.family}', var(--font-family);
-  --background: {resolveBackground(theme)};
-  --base-padding: {theme.spacing.padding}rem;
+  
+	--base-padding: {theme.spacing.padding}rem;
   --base-gap: {theme.spacing.gap}rem;
-  --widgets-background-color: {theme.widgets.color_background};
+  
+	--widgets-background-color: {theme.widgets.color_background};
   --widgets-background-color-dim: {theme.widgets.color_background_dim};
-  --widgets-border-width: {theme.widgets.border?.width}px;
-  --widgets-border-color: {theme.widgets.border?.color};
-  --widgets-border-base-radius: {theme.widgets.border?.radius}rem;
+  --widgets-border-width: {theme.widgets.border.width}px;
+  --widgets-border-color: {theme.widgets.border.color};
+  --widgets-border-base-radius: {theme.widgets.border.radius}rem;
   --widgets-box-shadow-x: {theme.widgets.shadow?.x}px;
   --widgets-box-shadow-y: {theme.widgets.shadow?.y}px;
   --widgets-box-shadow-blur: {theme.widgets.shadow?.blur}px;
   --widgets-box-shadow-spread: {theme.widgets.shadow?.spread}px;
   --widgets-box-shadow-color: {theme.widgets.shadow?.color};
+	
+	--buttons-primary-background-color: {theme.primary_buttons.color_background};
+	--buttons-primary-on-background-color: {theme.primary_buttons.color_on_background};
+	--buttons-prumary-border-color: {theme.primary_buttons.border.color}
+	--buttons-primary-box-shadow-x: {theme.primary_buttons.shadow?.x}px;
+	--buttons-primary-box-shadow-y: {theme.primary_buttons.shadow?.y}px;
+	--buttons-primary-box-shadow-blur: {theme.primary_buttons.shadow?.blur}px;
+	--buttons-primary-box-shadow-spread: {theme.primary_buttons.shadow?.spread}px;
+	--buttons-primary-box-shadow-color: {theme.primary_buttons.shadow?.color};
+	
+	--inputs-border-width: {theme.secondary_inputs.border.width}px;
+	--inputs-border-base-radius: {theme.secondary_inputs.border.radius}rem;
+
+	--inputs-background-color: {theme.secondary_inputs.color_background};
+	--inputs-on-background-color: {theme.secondary_inputs.color_on_background};
+	--inputs-border-color: {theme.secondary_inputs.border.color};
+	--inputs-box-shadow-x: {theme.secondary_inputs.shadow?.x}px;
+	--inputs-box-shadow-y: {theme.secondary_inputs.shadow?.y}px;
+	--inputs-box-shadow-blur: {theme.secondary_inputs.shadow?.blur}px;
+	--inputs-box-shadow-spread: {theme.secondary_inputs.shadow?.spread}px;
+	--inputs-box-shadow-color: {theme.secondary_inputs.shadow?.color};
   "
 >
 	{@render children()}
