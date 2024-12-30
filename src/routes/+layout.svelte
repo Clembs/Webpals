42f38a98e2040309b8de4e21801491e233146fd8
Clément Voisin <clembs@clembs.com>
2024-12-30 18:30:27 +0100

    feat: add heartbeats back

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
 	import IconContext from 'phosphor-svelte/lib/IconContext';
 	import '../styles/global.scss';
-	// import { browser } from '$app/environment';
-	// import { HEARTBEAT_INTERVAL } from '$lib/helpers/constants';
+	import { browser } from '$app/environment';
+	import { HEARTBEAT_INTERVAL } from '$lib/helpers/constants';
 	import NavBar from '$lib/components/NavBar/NavBar.svelte';
 	import DialogPortal from '$lib/portals/DialogPortal.svelte';
 
 	let { data, children } = $props();
 
 	// if the user is not purposely offline, send a heartbeat to keep them online
-	// if (browser && data.currentUser && data.currentUser.status !== 'offline') {
-	// 	// send a heartbeat every 3 minutes to keep the user online
-	// 	setInterval(async () => {
-	// 		await fetch('/api/heartbeat', {
-	// 			method: 'POST'
-	// 		});
-	// 	}, HEARTBEAT_INTERVAL);
-	// }
+	if (browser && data.currentUser && data.currentUser.status !== 'offline') {
+		// send a heartbeat every 3 minutes to keep the user online
+		setInterval(async () => {
+			await fetch('/api/heartbeat', {
+				method: 'POST'
+			});
+		}, HEARTBEAT_INTERVAL);
+	}
 </script>
 
 <IconContext
