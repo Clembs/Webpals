11c4b172d60dbd5a643d578001e58c72fcae9024
Clément Voisin <clembs@clembs.com>
2025-01-01 14:22:57 +0100

    feat: organize friends in a grid layout

--- src/lib/widgets/default/FriendsWidgetComponent.svelte
+++ src/lib/widgets/default/FriendsWidgetComponent.svelte
@@ -26,47 +26,81 @@
 <BaseWidget {user} {widget} editingMode={editing}>
 	<h2>Friends ({friends.length})</h2>
 
-	<ul class="friends">
-		{#each friends as friend}
-			<li class="friend">
-				<a href="/{friend.username}">
-					<Avatar user={friend} size="48px" />
+	<div class="friends-wrapper">
+		<ul class="friends">
+			{#each friends.slice(0, 6) as friend}
+				<li class="friend">
+					<a class="friend-el" href="/{friend.username}">
+						<Avatar user={friend} size="48px" />
 
-					<div class="text">
-						<div class="display-name heading">
-							{friend.displayName || friend.username}
+						<div class="text">
+							<div class="display-name heading">
+								{friend.displayName || friend.username}
+							</div>
+							<div class="username subtext">
+								@{friend.username}
+							</div>
 						</div>
-						<div class="username subtext">
-							@{friend.username}
-						</div>
-					</div>
-				</a>
+					</a>
+				</li>
+			{/each}
+		</ul>
+		<!-- TODO: make clickable -->
+		{#if friends.length > 6}
+			<li class="all-friends">
+				<div class="friend-el" aria-disabled={true}>
+					<div class="display-name heading">All friends</div>
+				</div>
 			</li>
-		{/each}
-	</ul>
+		{/if}
+	</div>
 </BaseWidget>
 
 <style lang="scss">
+	.friends-wrapper {
+		display: flex;
+		flex-direction: column;
+		gap: var(--inputs-border-width);
+		background-color: var(--widgets-border-color);
+		border-radius: calc(var(--widgets-border-base-radius) * 0.5);
+		overflow: hidden;
+
+		.all-friends {
+			// TODO: make clickable
+			.friend-el {
+				cursor: not-allowed;
+				opacity: 0.75;
+				padding: var(--base-padding);
+			}
+		}
+	}
+
 	.friends {
 		display: grid;
-		// grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
-		gap: var(--base-gap);
-		background-color: var(--widgets-background-color-dim);
-		border-radius: calc(var(--widgets-border-base-radius) * 0.5);
+		grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+
+		gap: var(--inputs-border-width);
 		list-style: none;
+	}
 
-		.friend a {
+	.friend-el {
+		background-color: var(--widgets-background-color-dim);
+		display: flex;
+		align-items: center;
+		padding: calc(var(--base-padding) * 0.5) calc(var(--base-padding) * 0.75);
+		gap: calc(var(--base-gap) * 0.5);
+		text-decoration: none;
+		align-items: center;
+		height: 100%;
+
+		&:hover {
+			filter: brightness(0.95);
+		}
+
+		.text {
 			display: flex;
-			padding: calc(var(--base-padding) * 0.5) calc(var(--base-padding) * 0.75);
-			gap: calc(var(--base-gap) * 0.5);
-			text-decoration: none;
-			align-items: center;
-
-			.text {
-				display: flex;
-				flex-direction: column;
-				// gap: calc(var(--base-gap) * 0.25);
-			}
+			flex-direction: column;
+			gap: calc(var(--base-gap) * 0.125);
 		}
 	}
 </style>
