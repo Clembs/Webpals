7573af2431b0855099566eeb070fc310dae325bc
Clément Voisin <clembs@clembs.com>
2025-02-17 02:55:52 +0100

    fix: use phosphor-svelte v3 vite optimizer tool

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { phosphorSvelteOptimize } from 'phosphor-svelte/preprocessor';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [phosphorSvelteOptimize(), vitePreprocess()],
+	preprocess: [vitePreprocess()],
 
 	kit: {
 		adapter: adapter({
