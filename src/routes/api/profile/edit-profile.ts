c7972cd991a47336babbe0130b39f8af3f65b0e8
Clément Voisin <clembs@clembs.com>
2025-02-17 03:06:02 +0100

    fix: remove accidental avatar input

--- src/routes/api/profile/edit-profile.ts
+++ src/routes/api/profile/edit-profile.ts
@@ -3,9 +3,9 @@ import type { RequestEvent } from './$types';
 import { db } from '$lib/db';
 import { profiles } from '$lib/db/schema/users';
 import { eq } from 'drizzle-orm';
-import sharp from 'sharp';
-import { clientSupabase } from '$lib/db/supabase';
-import { generateSnowflake } from '$lib/helpers/users';
+// import sharp from 'sharp';
+// import { clientSupabase } from '$lib/db/supabase';
+// import { generateSnowflake } from '$lib/helpers/users';
 
 export async function editProfile({ locals: { getCurrentProfile }, request }: RequestEvent) {
 	const profile = getCurrentProfile();
@@ -15,7 +15,7 @@ export async function editProfile({ locals: { getCurrentProfile }, request }: Re
 	const formData = await request.formData();
 	const displayName = formData.get('display-name')?.toString();
 	const pronouns = formData.get('pronouns')?.toString();
-	const avatar = formData.get('avatar') as File | null;
+	// const avatar = formData.get('avatar') as File | null;
 
 	if (!displayName || displayName.length >= 50) {
 		return fail(400, {
@@ -29,57 +29,57 @@ export async function editProfile({ locals: { getCurrentProfile }, request }: Re
 		});
 	}
 
-	if (avatar && !avatar.type.startsWith('image/')) {
-		return fail(400, {
-			message: 'Invalid avatar. Must be an image.'
-		});
-	}
+	// if (avatar && !avatar.type.startsWith('image/')) {
+	// 	return fail(400, {
+	// 		message: 'Invalid avatar. Must be an image.'
+	// 	});
+	// }
 
-	if (avatar && avatar.size > 1024 * 1024) {
-		return fail(400, {
-			message: 'Invalid avatar. Must be less than 1MB.'
-		});
-	}
+	// if (avatar && avatar.size > 1024 * 1024) {
+	// 	return fail(400, {
+	// 		message: 'Invalid avatar. Must be less than 1MB.'
+	// 	});
+	// }
 
-	let avatarId = profile.avatar;
+	// let avatarId = profile.avatar;
 
-	if (avatar && avatar.size > 0 && avatar.name) {
-		const resized = await sharp(await avatar.arrayBuffer())
-			.resize(128, 128, {
-				fit: 'cover'
-			})
-			.webp({
-				quality: 75
-			})
-			.toBuffer();
+	// if (avatar && avatar.size > 0 && avatar.name) {
+	// 	const resized = await sharp(await avatar.arrayBuffer())
+	// 		.resize(128, 128, {
+	// 			fit: 'cover'
+	// 		})
+	// 		.webp({
+	// 			quality: 75
+	// 		})
+	// 		.toBuffer();
 
-		if (profile.avatar) {
-			await clientSupabase.storage.from('avatars').remove([`${profile.id}/${avatarId}`]);
-		}
+	// 	if (profile.avatar) {
+	// 		await clientSupabase.storage.from('avatars').remove([`${profile.id}/${avatarId}`]);
+	// 	}
 
-		avatarId = generateSnowflake();
+	// 	avatarId = generateSnowflake();
 
-		const { error } = await clientSupabase.storage
-			.from('avatars')
-			.upload(`${profile.id}/${avatarId}.webp`, resized, {
-				contentType: 'image/webp',
-				cacheControl: 'public, max-age=31536000, immutable'
-			});
+	// 	const { error } = await clientSupabase.storage
+	// 		.from('avatars')
+	// 		.upload(`${profile.id}/${avatarId}.webp`, resized, {
+	// 			contentType: 'image/webp',
+	// 			cacheControl: 'public, max-age=31536000, immutable'
+	// 		});
 
-		if (error) {
-			console.error(error);
-			return fail(500, {
-				message: 'Failed to upload avatar.'
-			});
-		}
-	}
+	// 	if (error) {
+	// 		console.error(error);
+	// 		return fail(500, {
+	// 			message: 'Failed to upload avatar.'
+	// 		});
+	// 	}
+	// }
 
 	await db
 		.update(profiles)
 		.set({
 			displayName,
-			pronouns,
-			avatar: avatarId
+			pronouns
+			// avatar: avatarId
 		})
 		.where(eq(profiles.id, profile.id));
 
