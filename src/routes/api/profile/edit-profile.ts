b7dfffc0b9bc494479caa1d68122ea518afecfd2
Clément Voisin <clembs@clembs.com>
2025-02-21 13:46:40 +0100

    feat: server side avatar checking & uploading

--- src/routes/api/profile/edit-profile.ts
+++ src/routes/api/profile/edit-profile.ts
@@ -3,11 +3,13 @@ import type { RequestEvent } from './$types';
 import { db } from '$lib/db';
 import { profiles } from '$lib/db/schema/profiles';
 import { eq } from 'drizzle-orm';
-// import sharp from 'sharp';
-// import { clientSupabase } from '$lib/db/supabase';
-// import { generateSnowflake } from '$lib/helpers/users';
+import sharp from 'sharp';
+import { generateSnowflake } from '$lib/helpers/users';
 
-export async function editProfile({ locals: { getCurrentProfile }, request }: RequestEvent) {
+export async function editProfile({
+	locals: { supabase, getCurrentProfile },
+	request
+}: RequestEvent) {
 	const profile = getCurrentProfile();
 
 	if (!profile) redirect(302, '/login');
@@ -15,7 +17,8 @@ export async function editProfile({ locals: { getCurrentProfile }, request }: Re
 	const formData = await request.formData();
 	const displayName = formData.get('display-name')?.toString();
 	const pronouns = formData.get('pronouns')?.toString();
-	// const avatar = formData.get('avatar') as File | null;
+	const avatar = formData.get('avatar') as File | undefined;
+	const hasUpdatedAvatar = avatar && avatar.size > 0;
 
 	if (!displayName || displayName.length >= 50) {
 		return fail(400, {
@@ -29,57 +32,66 @@ export async function editProfile({ locals: { getCurrentProfile }, request }: Re
 		});
 	}
 
-	// if (avatar && !avatar.type.startsWith('image/')) {
-	// 	return fail(400, {
-	// 		message: 'Invalid avatar. Must be an image.'
-	// 	});
-	// }
+	if (hasUpdatedAvatar && !avatar.type.startsWith('image/')) {
+		return fail(400, {
+			message: 'Please pick an image file for your avatar.'
+		});
+	}
 
-	// if (avatar && avatar.size > 1024 * 1024) {
-	// 	return fail(400, {
-	// 		message: 'Invalid avatar. Must be less than 1MB.'
-	// 	});
-	// }
+	if (
+		hasUpdatedAvatar &&
+		['image/jpeg', 'image/png', 'image/webp', 'image/gif', 'image/avif'].indexOf(avatar.type) === -1
+	) {
+		return fail(400, {
+			message: 'Please use a JPEG, PNG, WebP, GIF or AVIF format image for your avatar.'
+		});
+	}
 
-	// let avatarId = profile.avatar;
+	if (hasUpdatedAvatar && avatar.size > 1024 * 1024) {
+		return fail(400, {
+			message: "Please pick an avatar image that's less than 1MB."
+		});
+	}
 
-	// if (avatar && avatar.size > 0 && avatar.name) {
-	// 	const resized = await sharp(await avatar.arrayBuffer())
-	// 		.resize(128, 128, {
-	// 			fit: 'cover'
-	// 		})
-	// 		.webp({
-	// 			quality: 75
-	// 		})
-	// 		.toBuffer();
+	let avatarId = profile.avatar;
 
-	// 	if (profile.avatar) {
-	// 		await clientSupabase.storage.from('avatars').remove([`${profile.id}/${avatarId}`]);
-	// 	}
+	if (hasUpdatedAvatar && avatar.name) {
+		const resized = await sharp(await avatar.arrayBuffer())
+			.resize(128, 128, {
+				fit: 'cover'
+			})
+			.webp({
+				quality: 75
+			})
+			.toBuffer();
 
-	// 	avatarId = generateSnowflake();
+		if (profile.avatar) {
+			await supabase.storage.from('avatars').remove([`${profile.id}/${avatarId}`]);
+		}
 
-	// 	const { error } = await clientSupabase.storage
-	// 		.from('avatars')
-	// 		.upload(`${profile.id}/${avatarId}.webp`, resized, {
-	// 			contentType: 'image/webp',
-	// 			cacheControl: 'public, max-age=31536000, immutable'
-	// 		});
+		avatarId = generateSnowflake();
 
-	// 	if (error) {
-	// 		console.error(error);
-	// 		return fail(500, {
-	// 			message: 'Failed to upload avatar.'
-	// 		});
-	// 	}
-	// }
+		const { error } = await supabase.storage
+			.from('avatars')
+			.upload(`${profile.id}/${avatarId}.webp`, resized, {
+				contentType: 'image/webp',
+				cacheControl: 'public, max-age=31536000, immutable'
+			});
+
+		if (error) {
+			console.error(error);
+			return fail(500, {
+				message: 'Failed to upload avatar.'
+			});
+		}
+	}
 
 	await db
 		.update(profiles)
 		.set({
 			displayName,
-			pronouns
-			// avatar: avatarId
+			pronouns,
+			avatar: avatarId
 		})
 		.where(eq(profiles.id, profile.id));
 
